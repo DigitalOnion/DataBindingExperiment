37aa61301d75a7809e8f3398d2ca44bb61c80392
DigitalOnion <luis.viruena@gmail.com>
2017-06-18 13:14:21 -0700

    Add WebView support with URLs to AWS S3

--- app/src/main/java/com/outspace/databindingagain/MainActivity.java
+++ app/src/main/java/com/outspace/databindingagain/MainActivity.java
@@ -2,6 +2,9 @@ package com.outspace.databindingagain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
             String[] split = strExamples[i].split("\\|");
             example.classname = split[0];
             example.message   = split[1];
+            example.url       = split[2];
             examples[i] = example;
         }
 
@@ -39,10 +43,22 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String url = MainActivity.this.examples[position].url;
+                WebView web = (WebView) findViewById(R.id.web);
+                web.loadUrl(url);
+            }
+
+            @Override public void onNothingSelected(AdapterView<?> parent) { }
+        });
     }
 
     private class Example {
         public String classname;
         public String message;
+        public String url;
     }
 }
