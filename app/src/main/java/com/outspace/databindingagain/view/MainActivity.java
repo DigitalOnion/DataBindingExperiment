39eb1684757a03a043bb5c93d49cb16b15f452d0
DigitalOnion <luis.viruena@gmail.com>
2017-06-19 22:47:54 -0700

    Refactor methods contained in View (MainActivity) to the ViewModel (MainViewModel).

--- app/src/main/java/com/outspace/databindingagain/view/MainActivity.java
+++ app/src/main/java/com/outspace/databindingagain/view/MainActivity.java
@@ -18,23 +18,11 @@ import com.outspace.databindingagain.viewmodel.MainViewModel;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private Example[] examples = null;
     private String selectedClassName = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        String[] strExamples = getResources().getStringArray(R.array.examples);
-        examples = new Example[strExamples.length];
-        for(int i = 0; i < strExamples.length; i++) {
-            Example example = new Example();
-            String[] split = strExamples[i].split("\\|");
-            example.classname = split[0];
-            example.message   = split[1];
-            example.url       = split[2];
-            examples[i] = example;
-        }
 
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         MainViewModel mainVM = new MainViewModel(this, binding);
@@ -45,46 +33,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        ArrayList<String> array = new ArrayList<>();
-        for(Example ex: examples)
-            array.add(ex.message);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                R.layout.support_simple_spinner_dropdown_item, array);
-        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        spinner.setAdapter(adapter);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String url = MainActivity.this.examples[position].url;
-                WebView web = (WebView) findViewById(R.id.web);
-                web.loadUrl(url);
-                MainActivity.this.selectedClassName = null;
-                if(position != 0)
-                    MainActivity.this.selectedClassName = MainActivity.this.examples[position].classname;
-            }
-
-            @Override public void onNothingSelected(AdapterView<?> parent) { }
-        });
     }
 
-    public void OnClickBtnGo(View view) {
-        if(selectedClassName != null) {
-            try {
-                Class clazz = Class.forName(selectedClassName);
-                Intent intent = new Intent(this, clazz);
-                startActivity(intent);
-            } catch (ClassNotFoundException e) {
-                String notYet = getResources().getString(R.string.not_yet);
-                Toast.makeText(this, notYet, Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    private class Example {
-        public String classname;
-        public String message;
-        public String url;
-    }
 }
--- app/src/main/java/com/outspace/databindingagain/viewmodel/MainViewModel.java
+++ app/src/main/java/com/outspace/databindingagain/viewmodel/MainViewModel.java
@@ -1,27 +1,98 @@
 package com.outspace.databindingagain.viewmodel;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.outspace.databindingagain.R;
 import com.outspace.databindingagain.databinding.ActivityMainBinding;
+import com.outspace.databindingagain.view.MainActivity;
+
+import java.util.ArrayList;
 
 /**
  * Created by user on 6/18/17.
  */
 
 public class MainViewModel {
-    private Activity activity;
+    private AppCompatActivity activity;
     private ActivityMainBinding binding;
+    private WebView web;
+    private String selectedClassName;
+    private Example[] examples = null;
 
-    public MainViewModel(Activity activity, ActivityMainBinding binding) {
+    public MainViewModel(AppCompatActivity activity, ActivityMainBinding binding) {
         this.activity = activity;
         this.binding  = binding;
+
+        Spinner spinner = binding.spinner;
+        InitSpinner(spinner);
+        InitSpinnerListener(spinner);
     }
+
+//    public void OnClickBtnGo(View view) {
+//        Toast.makeText(activity.getApplicationContext(), "I am at OnClickBtnGo", Toast.LENGTH_SHORT).show();
+//    }
 
     public void OnClickBtnGo(View view) {
-        Toast.makeText(activity.getApplicationContext(), "I am at OnClickBtnGo", Toast.LENGTH_SHORT).show();
+        if(selectedClassName != null) {
+            try {
+                Class clazz = Class.forName(selectedClassName);
+                Intent intent = new Intent(activity, clazz);
+                activity.startActivity(intent);
+            } catch (ClassNotFoundException e) {
+                String notYet = activity.getResources().getString(R.string.not_yet);
+                Toast.makeText(activity, notYet, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    private void InitSpinner(Spinner spinner) {
+
+        ArrayList<String> array = new ArrayList<>();
+
+        String[] strExamples = activity.getResources().getStringArray(R.array.examples);
+        examples = new Example[strExamples.length];
+        for(int i = 0; i < strExamples.length; i++) {
+            Example example = new Example();
+            String[] split = strExamples[i].split("\\|");
+            example.classname = split[0];
+            example.message   = split[1];
+            example.url       = split[2];
+            examples[i] = example;
+            array.add(example.message);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(activity.getApplicationContext(),
+                R.layout.support_simple_spinner_dropdown_item, array);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
+    private void InitSpinnerListener(Spinner spinner) {
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String url = MainViewModel.this.examples[position].url;
+                MainViewModel.this.web.loadUrl(url);
+                MainViewModel.this.selectedClassName = null;
+                if(position != 0)
+                    MainViewModel.this.selectedClassName = MainViewModel.this.examples[position].classname;
+            }
 
+            @Override public void onNothingSelected(AdapterView<?> parent) { }
+        });
+
+    }
+
+    private class Example {
+        public String classname;
+        public String message;
+        public String url;
+    }
 }
