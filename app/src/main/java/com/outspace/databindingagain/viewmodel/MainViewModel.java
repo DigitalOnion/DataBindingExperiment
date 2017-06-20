1b70ef992445874380700fd7b001b163bad72ff9
DigitalOnion <luis.viruena@gmail.com>
2017-06-19 21:58:54 -0700

    Basic Implementation of the MainViewModel with only one method

--- app/src/main/java/com/outspace/databindingagain/viewmodel/MainViewModel.java
+++ app/src/main/java/com/outspace/databindingagain/viewmodel/MainViewModel.java
@@ -1,12 +1,26 @@
 package com.outspace.databindingagain.viewmodel;
 
+import android.app.Activity;
+import android.view.View;
+import android.widget.Toast;
+
 /**
  * Created by user on 6/18/17.
  */
 
 public class MainViewModel {
 
+    private Activity activity;
 
+    public MainViewModel() { }
+
+    public MainViewModel(Activity activity) {
+        this.activity = activity;
+    }
+
+    public void OnClickBtnGo(View view) {
+        Toast.makeText(activity.getApplicationContext(), "I am at OnClickBtnGo", Toast.LENGTH_SHORT).show();
+    }
 
 
 }
