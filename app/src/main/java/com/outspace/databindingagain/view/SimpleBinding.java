6167661816bdde0d7565a88b582017317190a225
DigitalOnion <luis.viruena@gmail.com>
2017-06-25 17:03:13 -0700

    Modify OnCreate to inflate from DataBindingUtil and add binding variable

--- app/src/main/java/com/outspace/databindingagain/view/SimpleBinding.java
+++ app/src/main/java/com/outspace/databindingagain/view/SimpleBinding.java
@@ -1,15 +1,27 @@
 package com.outspace.databindingagain.view;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.outspace.databindingagain.R;
+import com.outspace.databindingagain.databinding.ActivitySimpleBindingBinding;
+import com.outspace.databindingagain.model.Person;
 
 public class SimpleBinding extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_simple_binding);
+        //setContentView(R.layout.activity_simple_binding);
+
+        Person person = new Person();
+        person.firstName  = "Luis Jesús";
+        person.lastName   = "Virueña Silva";
+        person.birthDay   = "08/16/2017";
+        person.coolPhrase = "Follow the white rabbit";
+
+        ActivitySimpleBindingBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_simple_binding);
+        binding.setPerson(person);
     }
 }
