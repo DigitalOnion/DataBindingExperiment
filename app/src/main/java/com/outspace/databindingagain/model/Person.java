fc4e7da761fc1564a9825a078a7b31ec355d9fce
DigitalOnion <luis.viruena@gmail.com>
2017-06-25 18:41:54 -0700

    Encapsulate attributes and add getters and setters

--- app/src/main/java/com/outspace/databindingagain/model/Person.java
+++ app/src/main/java/com/outspace/databindingagain/model/Person.java
@@ -3,8 +3,40 @@ package com.outspace.databindingagain.model;
 // POJO for a base person's class.
 
 public class Person {
-    public String firstName;
-    public String lastName;
-    public String birthDay;
-    public String coolPhrase;
+    private String firstName;
+    private String lastName;
+    private String birthDay;
+    private String coolPhrase;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(String birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getCoolPhrase() {
+        return coolPhrase;
+    }
+
+    public void setCoolPhrase(String coolPhrase) {
+        this.coolPhrase = coolPhrase;
+    }
 }
