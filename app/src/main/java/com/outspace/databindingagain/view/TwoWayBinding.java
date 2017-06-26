b1201fbe22d0e4163e7a683b5ae7c73c5d35c0d1
DigitalOnion <luis.viruena@gmail.com>
2017-06-25 18:37:55 -0700

    Add Clear Button OnClickListener

--- app/src/main/java/com/outspace/databindingagain/view/TwoWayBinding.java
+++ app/src/main/java/com/outspace/databindingagain/view/TwoWayBinding.java
@@ -3,9 +3,11 @@ package com.outspace.databindingagain.view;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.outspace.databindingagain.R;
 import com.outspace.databindingagain.databinding.ActivitySimpleBindingBinding;
+import com.outspace.databindingagain.databinding.ActivityTwoWayBindingBinding;
 import com.outspace.databindingagain.model.Person;
 
 public class TwoWayBinding extends AppCompatActivity {
@@ -15,13 +17,23 @@ public class TwoWayBinding extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_simple_binding);
 
-        Person person = new Person();
+        final Person person = new Person();
         person.firstName  = "Luis Jesús";
         person.lastName   = "Virueña Silva";
         person.birthDay   = "08/16/2017";
         person.coolPhrase = "Follow the white rabbit";
 
-        ActivitySimpleBindingBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_two_way_binding);
+        ActivityTwoWayBindingBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_two_way_binding);
         binding.setPerson(person);
+
+        binding.btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                person.firstName  = "";
+                person.lastName   = "";
+                person.birthDay   = "";
+                person.coolPhrase = "";
+            }
+        });
     }
 }
