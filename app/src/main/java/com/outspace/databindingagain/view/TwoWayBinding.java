f268c9264d5063768a9f89b88da13c5944357937
DigitalOnion <luis.viruena@gmail.com>
2017-06-27 21:52:34 -0700

    TwoWayBinding working all right

--- app/src/main/java/com/outspace/databindingagain/view/TwoWayBinding.java
+++ app/src/main/java/com/outspace/databindingagain/view/TwoWayBinding.java
@@ -3,25 +3,29 @@ package com.outspace.databindingagain.view;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import com.outspace.databindingagain.R;
 import com.outspace.databindingagain.databinding.ActivitySimpleBindingBinding;
 import com.outspace.databindingagain.databinding.ActivityTwoWayBindingBinding;
 import com.outspace.databindingagain.model.Person;
+import com.outspace.databindingagain.model.PersonObservable;
 
 public class TwoWayBinding extends AppCompatActivity {
+
+    private PersonObservable person = new PersonObservable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_simple_binding);
 
-        final Person person = new Person();
-        person.firstName  = "Luis Jesús";
-        person.lastName   = "Virueña Silva";
-        person.birthDay   = "08/16/2017";
-        person.coolPhrase = "Follow the white rabbit";
+        person.setFirstName("Luis Jesús");
+        person.setLastName("Virueña Silva");
+        person.setBirthDay("08/16/2017");
+        person.setCoolPhrase("Follow the white rabbit");
 
         ActivityTwoWayBindingBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_two_way_binding);
         binding.setPerson(person);
@@ -29,10 +33,18 @@ public class TwoWayBinding extends AppCompatActivity {
         binding.btnClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                person.firstName  = "";
-                person.lastName   = "";
-                person.birthDay   = "";
-                person.coolPhrase = "";
+                person.setFirstName("");
+                person.setLastName("");
+                person.setBirthDay("");
+                person.setCoolPhrase("");
+            }
+        });
+
+        binding.btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = person.getFirstName();
+                Toast.makeText(TwoWayBinding.this, "Firstname:" + s, Toast.LENGTH_SHORT).show();
             }
         });
     }
